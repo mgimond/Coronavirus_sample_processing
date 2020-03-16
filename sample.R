19f1de60074edbff412af5bf50c6c52d04c9668c
Manny Gimond <mgimond@users.noreply.github.com>
2020-03-16 16:08:27 -0400

    Update sample.R

--- sample.R
+++ sample.R
@@ -12,7 +12,7 @@ conf1  <- conf %>%
   pivot_longer(names_to = "Date", values_to = "Counts", cols = -(1:4) ) %>% 
   mutate(Date = mdy(Date),
          label = "confirmed")
-  
+
 death1 <- death %>% 
   pivot_longer(names_to = "Date", values_to = "Counts", cols = -(1:4) ) %>% 
   mutate(Date = mdy(Date),
@@ -26,7 +26,8 @@ recov1  <- recov %>%
 dat <- bind_rows(conf1, death1, recov1)
 
 dat %>% group_by(Date, label) %>% summarise(count = sum(Counts)) %>% 
-  ggplot() + aes(x=Date, y=count, col = label) + geom_line() + geom_point() 
+  ggplot() + aes(x=Date, y=count, col = label) + geom_line() + geom_point() +
+  coord_trans(y = "log")
 
 
 #### Map
@@ -39,11 +40,21 @@ world <- ne_countries(scale = "small", returnclass = "sf") %>%
 
 dat2 <- dat %>% 
   filter(label == "confirmed", Date == max(Date)) %>% 
-  group_by(`Country/Region`) %>% 
-  summarise(cnt = sum(Counts))
+  mutate(Country = case_when(`Country/Region` == "US" ~ "United States of America",
+                             `Country/Region` %in% "Congo" ~ "Congo",
+                             TRUE ~ `Country/Region` )) %>% 
+  group_by(Country) %>% 
+  summarise(cnt = sum(Counts)) 
 
 world2 <- world %>% 
-  left_join(dat2, by = c("postal" = "Country/Region"))
+  left_join(dat2, by = c("admin" = "Country"))
 
 ggplot(world2) + geom_sf(aes(fill = cnt)) +
-  scale_fill_binned(low="yellow", high="red")
+  scale_fill_binned(#low="#FFFFCC", high="#800026", 
+                     gradient = RColorBrewer::brewer.pal(6, "Reds"),
+                     breaks = c(1000, 2000, 3000, 5000, 10000, 50000, 100000),
+                     guide = guide_coloursteps(even.steps = FALSE, 
+                                               show.limits = TRUE,
+                                               ticks.linewidth = 2,
+                                               barheight = unit(3, "in")))
+
